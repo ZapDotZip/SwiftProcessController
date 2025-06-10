ca7d04fe662574297c8d4a8f5b60f4bc5b81ec0e
zap.zip <zapdotzip@gmail.com>
2025-06-09 17:43:55 -0700

    add more ProcessController tests

--- SwiftProcessControllerTests/ProcessControllerTests.swift
+++ SwiftProcessControllerTests/ProcessControllerTests.swift
@@ -30,6 +30,20 @@ final class ProcessControllerTests: XCTestCase {
 		XCTAssertEqual(stderrData.count, 0)
 	}
 	
+	func testSimpleExitCode() throws {
+		var stdinData = Data()
+		let inHandler: pipedDataHandler = { d in stdinData.append(d) }
+		var stderrData = Data()
+		let errHandler: pipedDataHandler = { d in stderrData.append(d) }
+		var exitCode: Int32?
+		let termHandler: terminationHandler = { code in exitCode = code }
+		let pc = ProcessController(executablePath: "/usr/bin/false", stdoutHandler: inHandler, stderrHandler: errHandler, terminationHandler: termHandler)
+		try pc.launch(args: [])
+		XCTAssertEqual(exitCode, 1)
+		XCTAssertEqual(stdinData.count, 0)
+		XCTAssertEqual(stderrData.count, 0)
+	}
+
 	func testMultiLine() throws {
 		var stdinData = Data()
 		let inHandler: pipedDataHandler = { d in stdinData.append(d) }
@@ -59,4 +73,69 @@ final class ProcessControllerTests: XCTestCase {
 		XCTAssertEqual(stderrData.count, 0)
 	}
 	
+	func testSimpleCommand() throws {
+		var stdinData = Data()
+		let inHandler: pipedDataHandler = { d in stdinData.append(d) }
+		var stderrData = Data()
+		let errHandler: pipedDataHandler = { d in stderrData.append(d) }
+		var exitCode: Int32?
+		let termHandler: terminationHandler = { code in exitCode = code }
+		let pc = ProcessController(executablePath: "/bin/sh", stdoutHandler: inHandler, stderrHandler: errHandler, terminationHandler: termHandler)
+		let standardInput = Pipe()
+		pc.standardInput = standardInput
+		let dq = DispatchQueue(label: "test.async")
+		dq.async {
+			do {
+				try standardInput.fileHandleForWriting.write(contentsOf: "echo hello\n".data(using: .ascii)!)
+				try standardInput.fileHandleForWriting.write(contentsOf: "echo hello\n".data(using: .ascii)!)
+				try standardInput.fileHandleForWriting.close()
+			} catch {
+				XCTFail(error.localizedDescription)
+			}
+		}
+		try pc.launch(args: [])
+		XCTAssertEqual(exitCode, 0)
+		XCTAssertEqual(String(data: stdinData, encoding: .ascii), "hello\nhello\n")
+		XCTAssertEqual(stderrData.count, 0)
+	}
+	
+	func testMultiLineWithError() throws {
+		var stdinData = Data()
+		let inHandler: pipedDataHandler = { d in stdinData.append(d) }
+		var stderrData = Data()
+		let errHandler: pipedDataHandler = { d in stderrData.append(d) }
+		var exitCode: Int32?
+		let termHandler: terminationHandler = { code in exitCode = code }
+		let pc = ProcessController(executablePath: "/bin/sh", stdoutHandler: inHandler, stderrHandler: errHandler, terminationHandler: termHandler)
+		let input = Pipe()
+		pc.standardInput = input
+		let dq = DispatchQueue(label: "test.async")
+		var inResult = ""
+		var errResult = ""
+		dq.async {
+			do {
+				for i in 0...150 {
+					if ((i % 3) == 0) {
+						let inLine = "echo line \(i)\n"
+						try input.fileHandleForWriting.write(contentsOf: inLine.data(using: .ascii)!)
+						inResult += "line \(i)\n"
+					}
+					if ((i % 5) == 0) {
+						let errLine = "echo line \(i) >&2\n"
+						try input.fileHandleForWriting.write(contentsOf: errLine.data(using: .ascii)!)
+						errResult += "line \(i)\n"
+					}
+				}
+				try input.fileHandleForWriting.close()
+			} catch {
+				XCTFail(error.localizedDescription)
+			}
+		}
+		
+		try pc.launch(args: [])
+		XCTAssertEqual(exitCode, 0)
+		XCTAssertEqual(String(data: stdinData, encoding: .ascii), inResult)
+		XCTAssertEqual(String(data: stderrData, encoding: .ascii), errResult)
+	}
+
 }
